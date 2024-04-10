8d23955478b6f7a2149985ee9f931783ddf072c4
Gabrielteju~ <tejgabriel03@gmail.com>
2024-04-10 01:20:15 +0100

    continuation

--- first.py
+++ first.py
@@ -1,2 +1,60 @@
-print('my clearfirst code in python')
-print('run code for me boss')
+#INDENTATION
+# print('hello-world')
+## indentation is very important(to indicate blocks of codes) also take note of the 'colon'
+if 5>3:  
+    print('i am bored') 
+    print('five is greater than two')
+
+#COMMENT
+# Multiline comment
+    """
+        we are here boss
+    """
+#singleline comment
+    #i am a man
+   
+#VARIABLES
+    x=4 + 2   #output should be an integer
+    x='string' #it has now been reassigned to be a string
+    print(x)
+#CASTING(If you want to specify a data type of a variable)
+    t=str(3)
+    w=int(4)
+    z=float(2)
+    print(z)
+    print(w)
+    print(t)
+
+#GETTING THE DATA TYPE OF A VARIABLE
+    x=5
+    print(type(x))
+    y='the man of the match'
+    print(y)
+
+# String variables can be declared making use of single or double quotes
+#variable names cannot start with numbers, we cannot make use of hypen also nameing variables, other things that can work for naming variables is the underscore.
+
+    
+x,y,z=2,3,4   #python allows you to assign values to multiple variables, number of variables must match the number of values
+print(y)
+    
+t=u=i=4
+print(t)
+print(i)
+print(u)    #python allows you to assign multiple variables to a single value as seen above
+
+
+#UNPACKING, say
+fruits=['apple', 'banana', 'cherry']  #this is a list, as enclosed with a burly braces
+x,y,z=fruits
+print(y)
+
+#Python allows you to combine text and a variable
+the_string='radio'
+the_num='45'
+print(the_string + " " +the_num)
+
+#PYTHON GLOBAL VARIABLES
+
+
+
